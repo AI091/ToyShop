de7760975f185441070dacb65442d118676df82f
Ahmed Ibrahem <ahmedibrahem00091@gmaill.com>
2023-01-10 00:52:16 +0200

    heroku deployment

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -10,7 +10,8 @@ router.get('/',authMiddleWare, cartController.getItems);
 router.post('/add-item',authMiddleWare, cartController.addItem);
 router.post('/delete-item',authMiddleWare, cartController.deleteItem);
 router.post('/update-quantity',authMiddleWare, cartController.updateQuantity);
-
+router.post('/place-order',authMiddleWare, cartController.placeOrder);
+router.delete('/clear-cart',authMiddleWare, cartController.clearCart)
 // define route to catch the items under which brand
 // router.get('/brand/:brand', itemsController.getItemsByBrand);
 
